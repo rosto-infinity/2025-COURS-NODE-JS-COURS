7ccb59e46012c3d24c263364781bf9c48dca9232
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-20 04:31:26 +0100

    message

--- module05/app.js
+++ module05/app.js
@@ -4,5 +4,5 @@ const app = express();
 app.get("/post", (req, res) => {
   res.json({message : "Voici les données envoyer ok !"})
 })
-// Lancer le serveur
-app.listen(port, () => console.log("Le serveur a démarré au port  " + port));
+// Lancer le serveur au port 5000
+app.listen(port, () => console.log("Le serveur a démarré avec succèss au port  " + port));
