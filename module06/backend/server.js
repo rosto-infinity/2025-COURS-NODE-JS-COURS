87b5c8b78809bd270f9bc90447b950273f818ec0
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-26 14:33:50 +0100

    post

--- module06/backend/server.js
+++ module06/backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 const port = 3001;
-const app = new express();
+const app =  express();
 
+app.get('/post', (req, res) => {
+  res.send('Hello World! les dev');
+});
 //lancer le serveur
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port} avec succès ok`);
