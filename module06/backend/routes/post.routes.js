4c58b743e0cf9493cfaff5941dc778988f2ffbbb
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-04 09:24:42 +0100

    patch

--- module06/backend/routes/post.routes.js
+++ module06/backend/routes/post.routes.js
@@ -37,19 +37,27 @@ router.delete('/:id', (req, res) => {
 });
 
 // Route PATCH avec ID
-router.patch('/:id', (req, res) => {
-  const id = req.params.id; // Récupérer l'ID depuis l'URL
-  const updates = req.body; // Récupérer les données à mettre à jour depuis le corps de la requête
+// router.patch('/:id', (req, res) => {
+//   const id = req.params.id; // Récupérer l'ID depuis l'URL
+//   const updates = req.body; // Récupérer les données à mettre à jour depuis le corps de la requête
 
-  // Logique pour mettre à jour la ressource avec l'ID
-  // Remplacez ceci par votre logique de mise à jour (par exemple, mise à jour dans une base de données)
+//   // Logique pour mettre à jour la ressource avec l'ID
+//   // Remplacez ceci par votre logique de mise à jour (par exemple, mise à jour dans une base de données)
 
+//   res.json({
+//     id: id, // ID de la ressource mise à jour
+//     updates: updates, // Les données mises à jour
+//     status: 'Updated successfully' // Indicateur de succès
+//   });
+// });
+
+
+
+router.patch("/like-post/:id", (req, res) => {
   res.json({
-    id: id, // ID de la ressource mise à jour
-    updates: updates, // Les données mises à jour
-    status: 'Updated successfully' // Indicateur de succès
+    message: ` Post ${req.params.id} liked successfully`
   });
-});
+  });
 export default router; // cas : ES6
 
 // module.exports = router;  cas require: CommonJS
