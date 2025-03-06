3ec24b74dbfcfb1d6477e68d8c4ec2e16811a953
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-06 11:17:16 +0100

    add model

--- module06/backend/config/database.js
+++ module06/backend/config/database.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = () => {
-  // mongoose.set("strictQuery", false);
   mongoose.connect(process.env.MongoDB_URI)
     .then(() => {
       console.log("Connexion à la base de données réussie");
     })
-    // .catch((err) => {
-    //   console.log("Connexion à la base de données échouée", err);
-    // });
 };
 export default connectDB;
---
+++ module06/backend/model/post.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const postSchema = mongoose.Schema(
+  {
+    message: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    likers: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export default mongoose.model("Post", postSchema);
+// module.exports = mongoose.model('Post', postSchema);
